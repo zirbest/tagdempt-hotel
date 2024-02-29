9463926e17e5570d31663701c9be21c1cc63594a
zirbest <zirbst@gmail.com>
2024-02-29 01:22:37 +0100

    chore(shadcn): update combobox

--- src/renderer/src/components/ui/combobox.tsx
+++ src/renderer/src/components/ui/combobox.tsx
@@ -1,95 +1,97 @@
-import type { Component } from "solid-js"
-import { splitProps } from "solid-js"
+import { cn } from "@/lib/utils";
+import { Combobox as ComboboxPrimitive } from "@kobalte/core";
+import type { VoidComponent } from "solid-js";
+import { splitProps, type ParentComponent } from "solid-js";
 
-import { Combobox as ComboboxPrimitive } from "@kobalte/core"
+export const Combobox = ComboboxPrimitive.Root;
+export const ComboboxDescription = ComboboxPrimitive.Description;
+export const ComboboxErrorMessage = ComboboxPrimitive.ErrorMessage;
+export const ComboboxItemDescription = ComboboxPrimitive.ItemDescription;
+export const ComboboxHiddenSelect = ComboboxPrimitive.HiddenSelect;
+export type ComboboxTriggerMode = ComboboxPrimitive.ComboboxTriggerMode;
 
-import { Icons } from "../icons"
-import { cn } from "~/lib/utils"
-
-const Combobox = ComboboxPrimitive.Root
-const ComboboxControl = ComboboxPrimitive.Control
-const ComboboxDescription = ComboboxPrimitive.Description
-const ComboboxErrorMessage = ComboboxPrimitive.ErrorMessage
-const ComboboxItemDescription = ComboboxPrimitive.ItemDescription
-const ComboboxHiddenSelect = ComboboxPrimitive.HiddenSelect
-type ComboboxTriggerMode = ComboboxPrimitive.ComboboxTriggerMode
-
-const ComboboxInput: Component<ComboboxPrimitive.ComboboxInputProps> = (props) => {
-  const [, rest] = splitProps(props, ["class"])
+export const ComboboxInput: VoidComponent<ComboboxPrimitive.ComboboxInputProps> = props => {
+  const [local, rest] = splitProps(props, ["class", "children"]);
   return (
     <ComboboxPrimitive.Input
       class={cn(
-        "placeholder:text-muted-foreground h-full bg-transparent text-sm focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
-        props.class
+        "h-full bg-transparent text-sm placeholder:text-muted-foreground focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        local.class
       )}
       {...rest}
     />
-  )
-}
+  );
+};
 
-const ComboboxTrigger: Component<ComboboxPrimitive.ComboboxTriggerProps> = (props) => {
-  const [, rest] = splitProps(props, ["class", "children"])
+export const ComboboxTrigger: ParentComponent<ComboboxPrimitive.ComboboxTriggerProps> = props => {
+  const [local, rest] = splitProps(props, ["class", "children"]);
   return (
-    <ComboboxPrimitive.Trigger
-      class={cn(
-        "border-input flex h-9 w-full items-center justify-between rounded-md border px-3 shadow-sm",
-        props.class
-      )}
-      {...rest}
-    >
-      {props.children}
-      <ComboboxPrimitive.Icon class="flex h-3.5 w-3.5 items-center justify-center">
-        <Icons.chevronsUpDown class="h-4 w-4 opacity-50" />
-      </ComboboxPrimitive.Icon>
-    </ComboboxPrimitive.Trigger>
-  )
-}
+    <ComboboxPrimitive.Control>
+      <ComboboxPrimitive.Trigger
+        class={cn(
+          "flex h-9 w-full items-center justify-between rounded-md border border-input px-3 shadow-sm",
+          local.class
+        )}
+        {...rest}
+      >
+        {local.children}
+        <ComboboxPrimitive.Icon class="flex h-3.5 w-3.5 items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-4 w-4 opacity-50">
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m8 9l4-4l4 4m0 6l-4 4l-4-4"
+            />
+          </svg>
+        </ComboboxPrimitive.Icon>
+      </ComboboxPrimitive.Trigger>
+    </ComboboxPrimitive.Control>
+  );
+};
 
-const ComboboxContent: Component<ComboboxPrimitive.ComboboxContentProps> = (props) => {
-  const [, rest] = splitProps(props, ["class"])
+export const ComboboxContent: VoidComponent<ComboboxPrimitive.ComboboxContentProps> = props => {
+  const [local, rest] = splitProps(props, ["class"]);
   return (
     <ComboboxPrimitive.Portal>
       <ComboboxPrimitive.Content
         class={cn(
-          "bg-popover text-popover-foreground data-[expanded]:animate-in data-[closed]:animate-out data-[closed]:fade-out-0 data-[expanded]:fade-in-0 data-[closed]:zoom-out-95 data-[expanded]:zoom-in-95 relative z-50 min-w-[8rem] overflow-hidden rounded-md border shadow-md",
-          props.class
+          "relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[expanded]:animate-in data-[closed]:animate-out data-[closed]:fade-out-0 data-[expanded]:fade-in-0 data-[closed]:zoom-out-95 data-[expanded]:zoom-in-95",
+          local.class
         )}
         {...rest}
       >
         <ComboboxPrimitive.Listbox class="p-1" />
       </ComboboxPrimitive.Content>
     </ComboboxPrimitive.Portal>
-  )
-}
+  );
+};
 
-const ComboboxItem: Component<ComboboxPrimitive.ComboboxItemProps> = (props) => {
-  const [, rest] = splitProps(props, ["class", "children"])
+export const ComboboxItem: ParentComponent<ComboboxPrimitive.ComboboxItemProps> = props => {
+  const [local, rest] = splitProps(props, ["class", "classList", "children"]);
   return (
     <ComboboxPrimitive.Item
       class={cn(
-        "data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-        props.class
+        "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50",
+        local.class
       )}
       {...rest}
     >
       <ComboboxPrimitive.ItemIndicator class="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
-        <Icons.check class="h-4 w-4" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-4 w-4">
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m5 12l5 5L20 7"
+          />
+        </svg>
       </ComboboxPrimitive.ItemIndicator>
-      <ComboboxPrimitive.ItemLabel>{props.children}</ComboboxPrimitive.ItemLabel>
+      <ComboboxPrimitive.ItemLabel>{local.children}</ComboboxPrimitive.ItemLabel>
     </ComboboxPrimitive.Item>
-  )
-}
-
-export {
-  Combobox,
-  ComboboxControl,
-  ComboboxDescription,
-  ComboboxErrorMessage,
-  ComboboxItemDescription,
-  ComboboxHiddenSelect,
-  ComboboxInput,
-  ComboboxTrigger,
-  ComboboxContent,
-  ComboboxItem,
-  type ComboboxTriggerMode
-}
+  );
+};
